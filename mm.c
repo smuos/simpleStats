6bb82549c8bdfd2fa89e515f9b3f6a97bb374205
Fengxiao Yuan <fn708010@dal.ca>
2014-10-02 11:12:48 -0300

    This is the assignment 2 Calculate the mean and median

--- mm.c
+++ mm.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define debug 0
 
@@ -12,6 +14,37 @@ int numcmp (const void *a, const void *b) {
     return 0;
 }
 
+//Calculate mean
+float mean(int* list, int length) {
+    float me = 0;//the return value for mean functon
+    int counter;//number of elements
+    for (counter = 0; counter < length; counter++) {
+        me = me + list[counter];//add all numbers
+    }
+    me = me/length;//calculate the mean, easy
+    return me;
+}
+
+//Calculate median
+float median(int* list, int length) {
+    /*There are two situations for median number.
+      First situation is when the length is an even number,
+      the mean of (length/2)th and (length/2+1)th numbers is the median.
+      Second situation is when the length is an odd number,
+      the median is (length/2)th number. 
+    */
+    float med;//the return value for median
+    //Length is an even number
+    if (length%2==0) {
+	med = (list[length/2-1] + list[length/2])/2.0;
+    }
+
+    //Length is an odd number
+    else {
+    med = list[(length+1)/2-1];
+    }
+    return med; 
+}     
 int main(int argc, char *argv[]) {
 
     int i, length, *pt;
@@ -24,9 +57,9 @@ int main(int argc, char *argv[]) {
 
     // Determine amount of numbers from argc
     length = argc - 1;
-#if debug
+//#if debug
     fprintf(stderr, "%s: DEBUG: %d numbers were passed.\n", argv[0], length);
-#endif
+//#endif
 
     // Allocate memory for array of number (and error check)
     if ((pt = malloc(length * sizeof(int))) == NULL) {
@@ -47,6 +80,17 @@ int main(int argc, char *argv[]) {
         fprintf(stdout, "%d ", pt[i]);
     }
     fprintf(stdout, "\n%s: FIN. \n", argv[0]);
-
+    int rc  = fork();//slice off another process
+    if (rc < 0) {
+	fprintf(stderr,"Fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0) {
+   	 fprintf(stdout, "The mean is %.1f\n", mean(pt,length));
+    }
+    else if (rc > 0) {
+	 wait(NULL);//wait for the child end
+	 fprintf(stdout, "The median is %.1f\n", median(pt,length));
+    }
     return 0;
 }
